992d80b83ba94c7d4ccd207da3959e91a5d9e192
Matthew Kob <mattk@shack-vm-debian-desk1.kob>
2020-04-22 00:56:10 -0400

    fixed scripts path for new system

--- minecraft/backup.sh
+++ minecraft/backup.sh
@@ -2,8 +2,6 @@
 
 echo "############################ BEGIN #################################" >> backup.lg
 date >> backup.lg
-#script -q -c "scp -P 19222 mkob@kobmine.garbagefile.net:/home/mkob/minecraftbe/kobmine/backups/*.gz /home/mattk/backups/kobmine/" >> backup.lg
-#script -q -c "scp -P 19222 mkob@kobmine.garbagefile.net:/home/mkob/minecraftbe/kobcreate/backups/*.gz /home/mattk/backups/kobcreate/" >> backup.lg
 
 script -q -c "scp -P 19222 mkob@kobmine.garbagefile.net:/home/mkob/minecraftbe/backupAll/*.gz /home/mattk/backups/" >>
 backup.lg
@@ -12,12 +10,5 @@ backup.lg
 date >> backup.lg
 echo "############################ END #################################" >> backup.lg
 
-#sed -i -e 's/\r/\n/g' backup.lg
 
-#tar -zcvf "/home/mattk/backupstorage/$(date '+%Y-%m-%d').kobmine.backups.tar.gz" ./backups/kobmine
-#tar -zcvf "/home/mattk/backupstorage/$(date '+%Y-%m-%d').kobcreate.backups.tar.gz" ./backups/kobcreate
-
-tar -zcvf "/home/mattk/backupstorage/$(date '+%Y-%m-%d').ALL.backups.tar.gz" ./backups/
-
-# rm /home/mattk/backups/kobmine/*.gz
-# rm /home/mattk/backups/kobcreate/*.gz
+tar -zcvf "/home/mattk/backupstorage/$(date '+%Y-%m-%d').ALL.backups.tar.gz" /home/mattk/Backups/minecraft/
