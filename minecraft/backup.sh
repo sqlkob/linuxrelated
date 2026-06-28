345a4b3112b7fe7dd63462b84d48e09607857d22
Matthew Kob <sqlkob@hotmail.com>
2026-06-28 00:44:49 -0400

    sloppy (#3)

--- minecraft/backup.sh
+++ minecraft/backup.sh
@@ -1,24 +1,5 @@
-#!/usr/bin/bash
-whoami 
-
-ps -f
-
-echo "############################ BEGIN SCP #################################" 
-
-script -q -c "scp -P 19222 -i '/home/mattk/.ssh/id_mine_rsa' mkob@kobmine.garbagefile.net:/home/mkob/minecraftbe/backupAll/\*.gz /home/mattk/Backups/minecraft/" 
-
-echo "############################ END SCP #################################" 
-
-echo "############################ BEGIN local tar  #################################" 
-
-script -c "tar -zcvf '/home/mattk/backupstorage/$(date '+%Y-%m-%d_%H:%M:%S').ALL.backups.tar.gz' --absolute-names /home/mattk/Backups/minecraft/" 
-
-echo "############################ END local tar  #################################" 
-
-echo "############################ BEGIN local clean-up  #################################" 
-
-script -c "rm /home/mattk/Backups/minecraft/*.gz" 
-
-echo "############################ END local clean-up  #################################" 
-
-##end## 
+#!/bin/sh
+TIMESTAMP=$(date +%F-%H%M)
+BACKUP_DIR="/backups" # Replace with your own path
+VOLUME_NAME="minecraft_data"
+tar -zcvpf "${BACKUP_DIR}/minecraft_backup_$TIMESTAMP.tar.gz" --absolute-names /var/lib/docker/volumes/$VOLUME_NAME/_data
